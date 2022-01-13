52dc1a5236f86369d3eb7f820915ec8f8ee88068
cloudjumpercat <75819066+cloudjumpercat@users.noreply.github.com>
2022-01-13 12:52:06 -0600

    Create convert-to-markdown.sh

---
+++ convert-to-markdown.sh
@@ -0,0 +1,4 @@
+#!/bin/zsh
+
+#rename html files to remove the date so it's just the title of the post
+#need something that will select all the html files in the folder and convert them to separate markdown files
