b45342943eaf8e8998412c9f30ef938dadb66ece
cloudjumpercat <75819066+cloudjumpercat@users.noreply.github.com>
2022-01-14 15:55:27 -0600

    Update convert-to-markdown.sh
    Think I fixed it so it doesn't cut out the wrong part and only leave one file

--- convert-to-markdown.sh
+++ convert-to-markdown.sh
@@ -1,4 +1,8 @@
 #!/bin/zsh
 
-#rename html files to remove the date so it's just the title of the post
+#rename html files to remove the date so it's just the title of the post (MAKE SURE TO CHANGE THE DATE!)
+for file in *
+ do
+    echo mv "$file" "$(echo "$file" | cut -c 12-)"
+ done  
 #need something that will select all the html files in the folder and convert them to separate markdown files
